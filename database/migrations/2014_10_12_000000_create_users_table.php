f9009e2a7f488dc8233ae476f68cd3ce08a349ff
kallbuloso <kallbuloso@gmail.com>
2019-04-22 19:59:51 -0300

    database/migrations/2014_10_12_000000_create_users_table:

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('verified')->default(false);
             $table->string('password');
+            $table->boolean('verified')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
